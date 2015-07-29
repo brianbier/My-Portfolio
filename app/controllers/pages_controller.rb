74d0842b05b4aa521243b1620100ed831da0671d
Brian <briansbier@gmail.com>
2015-07-28 22:38:15 -0400

    new controller actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,9 +8,23 @@ class PagesController < ApplicationController
   
   def about
   end
-
-  def contact
-  end
   
+  def contact
+  	@signup = Signup.new
+  end
+
+	def create
+		@signup = Signup.new(signup_params)
+		if @signup.save
+			redirect_to '/'
+		else
+			redirect_to '/signup'
+		end
+	end
+
+private
+def signup_params
+	params.require(:signup).permit(:firstname, :email, :number)
+end
   
 end
