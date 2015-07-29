c847258edf5a622a43bc38893412596b76826340
Brian <briansbier@gmail.com>
2015-07-28 22:34:36 -0400

    update my database

--- db/migrate/20150729022857_create_signups.rb
+++ db/migrate/20150729022857_create_signups.rb
@@ -1,8 +1,11 @@
 class CreateSignups < ActiveRecord::Migration
   def change
     create_table :signups do |t|
-
-      t.timestamps
+    	t.string :firstname
+    	t.string :lastname
+    	t.string :email
+    	t.string :message
+      	t.timestamps
     end
   end
 end
